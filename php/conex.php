8e527e9b4b09ce890bb17a23a2648b379eec7557
BrayanDelgado <isosbrayan@gmail.com>
2024-01-18 15:23:47 -0500

    pagina

--- php/conex.php
+++ php/conex.php
@@ -1,4 +1,15 @@
-<?php 
-$conn = mysqli_connect("localhost","root","","patpa");
+<?php
+$servername = "sql212.infinityfree.com";
+$username = "epiz_34328927";
+$password = "p67bcpLswq";
+$database = "epiz_34328927_XXX"; 
+// Crear conexión
+$conn = new mysqli($servername, $username, $password, $database);
 
+// Verificar la conexión
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+
+echo "Conexión exitosa";
 ?>
